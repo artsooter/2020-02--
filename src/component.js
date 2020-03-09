d29831177396768df49aa9e69b367de72647a44f
artshooter <1515248321@qq.com>
2020-03-09 23:39:25 +0800

    实现前端路由切换时触发组件的函数，，，（通过搭配点击事件，相即触发vue,vuex,组件中的对应函数。）

--- src/component.js
+++ src/component.js
@@ -1,5 +1,84 @@
-export const Foo = { template: '<div>foo {{$route.params.id}}</div>' }
-export const Bar = { template: '<div>bar</div>' }
+
+export const Loading = {
+	template: ' <div class="loading"  >\
+	 				<canvas id="loadingCanvas" ></canvas>\
+				</div>\
+				',
+	methods: {
+		init:function(){
+			let l_a = new this.LoadingAnimation(document.getElementById("loadingCanvas"));
+			l_a.canvas.width=document.getElementsByClassName("loading")[0].offsetWidth;
+			l_a.canvas.height=document.getElementsByClassName("loading")[0].offsetHeight;
+			l_a.update=this.update.bind(l_a);//将组件methods中的方法，搭载到对象上
+			window.requestAnimationFrame(l_a.update)//函数方法从外部搭载上去)
+		},
+		
+		LoadingAnimation:function(canvas){
+			console.log (canvas);
+			this.canvas=canvas;
+			this.ctx=canvas.getContext('2d');
+			this.img = new Image();
+			this.img.src="laodingImg.png";
+			this.timer;//定时器
+			this.xPos=0;
+			this.xPosDiv=0;//div的移动控制
+			this.speed=3;
+			this.changeCorrect=0;
+			this.changeFlag=0;//动画帧切换
+			this.readyFlag=0;//数据准备完成,动画消失
+
+			this.runFrame={
+				0:[1854,2,88,94,0,0,50,75],
+				1:[1942,2,88,94,0,0,50,75]
+			};
+			this.update;
+		},
+		update:function(){
+			this.changeCorrect++;
+			this.changeFlag=this.changeCorrect>5?1:0;
+			if(this.changeCorrect>10) this.changeCorrect=0;
+			this.xPos+=this.speed;
+			this.ctx.clearRect(0,0,this.canvas.width,this.canvas.height)
+			this.ctx.drawImage(
+				this.img,
+				this.runFrame[this.changeFlag][0],
+				this.runFrame[this.changeFlag][1],
+				this.runFrame[this.changeFlag][2],
+				this.runFrame[this.changeFlag][3],
+				this.runFrame[this.changeFlag][4]+this.xPos,
+				this.runFrame[this.changeFlag][5],
+				this.runFrame[this.changeFlag][6],
+				this.runFrame[this.changeFlag][7]);
+			if(this.xPos<this.canvas.width/2){
+				window.requestAnimationFrame(this.update)
+			}
+			/*
+			this.img.onload = function(){
+				console.log(this)
+				this.ctx.drawImage(
+					this.img,
+					this.runFrame[this.changeFlag][0],
+					this.runFrame[this.changeFlag][1],
+					this.runFrame[this.changeFlag][2],
+					this.runFrame[this.changeFlag][3],
+					this.runFrame[this.changeFlag][4]+100,
+					this.runFrame[this.changeFlag][5]+100,
+					this.runFrame[this.changeFlag][6],
+					this.runFrame[this.changeFlag][7]);
+			}	
+			*/
+
+		},	
+	},
+}
+
+
+
+
+export const Bar = {
+	props:['inte'],
+	template: '<div>{{ inte }}</div>',
+}
 
 export const vue_content=  {
 	// vue_content 组件现在接受一个
--- src/js.js
+++ src/js.js
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-import {Foo,Bar} from './component.js';
+import {Loading,Bar} from './component.js';
 import {net} from './tool.js';
 import './css.css';
 import Vue from 'vue/dist/vue.js';
@@ -11,19 +11,22 @@ Vue.use(VueRouter);
 //如果使用模块化机制编程，要调用 Vue.use(VueRouter)
 
 const routes = [
-  { path: '/foo/:id', component: Foo },
-  { path: '/bar', component: Bar }
+  { path: '/loading', component: Loading },
+  { path: '/bar/:inte', component: Bar ,props:true}
 ]//$route.params中会储存已经传入的参数
 
 const router = new VueRouter({
-  routes // (缩写) 相当于 routes: routes
+  routes ,// (缩写) 相当于 routes: routes
 })
 
 const store = new Vuex.Store({//状态保存空间
   state: {
     count: 0
   },
-  mutations: {
+  mutations: {//中间管理方法
+    loadingInit:function(){  
+      Loading.methods.init();
+    },
     add:function (states) {
       states.count++;
       console.log(states.count)
@@ -36,14 +39,44 @@ const store = new Vuex.Store({//状态保存空间
 let app=new Vue({//vue总管理器
   router,
   data:{
+    vuedata:123,
     arr_interface:[],
   },
   methods: {
+    VueRouterInit:function(event){//vue实例入口
+      let ans=event.target.hash.match(/#\/([^\/]*)/)[1];//匹配出path
+      console.log(ans)
+      store.commit(`${ans}Init`);
+    },
     clickMessage:function(){
       store.commit('add');
+      console.log(router);
     }
   },
 }).$mount("#app");
-// 现在，应用已经启动了！
 
-console.log(net(getManage))
+// 程序入口
+window.onload=function(){
+  //app.init();
+  const ans=[1,2,3];
+  let a = 'hello';
+  let b = 'world';
+
+  let test=function(){
+      setTimeout(() => {
+        console.log("异步1")
+      }, 0);
+      console.log("同步1")
+    
+    new Promise(function(reslove,reject){
+      console.log("同步2")
+      setTimeout(() => {
+        console.log("异步2")
+      }, 0);
+      reslove(3);
+    }).then((result) => {
+        console.log(`异步${result}`)
+    })   
+     
+  }();
+}
